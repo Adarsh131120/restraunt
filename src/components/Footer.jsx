1f0c15a76a82d43d98e8c611696db24999ff3196
Adarsh Tiwari personal <adarshtiwari131120@gmail.com>
2024-08-17 23:43:02 +0530

    new

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Adarsh </div>
+          <div className="right">
+            <p>Gothapatna , IIIT bhubaneswar , Khurda , Malipada , 751003</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed by Adarsh Tiwari</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By CodeWithAdarsh @2024</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
